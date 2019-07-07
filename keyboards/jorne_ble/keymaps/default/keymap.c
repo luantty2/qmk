f713858f0c11800ec5677ed4d53f9710e41c41e9
joric <joric@users.noreply.github.com>
2019-07-07 05:44:06 +0500

    oled display from crkbd

--- keyboards/jorne_ble/keymaps/default/keymap.c
+++ keyboards/jorne_ble/keymaps/default/keymap.c
@@ -1,20 +1,3 @@
-/*
-Copyright 2018 Sekigon
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include QMK_KEYBOARD_H
 #include "app_ble_func.h"
 
@@ -22,6 +5,31 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   #include "ssd1306.h"
 #endif
 
+#include "lib/mode_icon_reader.c"
+#include "lib/layer_state_reader.c"
+#include "lib/host_led_state_reader.c"
+#include "lib/logo_reader.c"
+#include "lib/keylogger.c"
+#include "lib/timelogger.c"
+
+// define variables for reactive RGB
+bool TOG_STATUS = false;
+int RGB_current_mode;
+#ifdef RGBLIGHT_ENABLE
+//Following line allows macro to read current RGB settings
+extern rgblight_config_t rgblight_config;
+#endif
+
+const uint8_t is_master = IS_LEFT_HAND;
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+#define _QWERTY 0
+#define _LOWER 3
+#define _RAISE 4
+#define _ADJUST 16
 
 enum custom_keycodes {
     AD_WO_L = SAFE_RANGE, /* Start advertising without whitelist  */
@@ -55,13 +63,6 @@ enum custom_keycodes {
 
 
 extern keymap_config_t keymap_config;
-
-enum layer_number {
-    _QWERTY = 0,
-    _LOWER,
-    _RAISE,
-    _ADJUST
-};
 
 // Fillers to make layering more clear
 #define _______ KC_TRNS
@@ -134,17 +135,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
-
-
-// define variables for reactive RGB
-bool TOG_STATUS = false;
-int RGB_current_mode;
-#ifdef RGBLIGHT_ENABLE
-//Following line allows macro to read current RGB settings
-extern rgblight_config_t rgblight_config;
-#endif
-
-
 void persistent_default_layer_set(uint16_t default_layer) {
   eeconfig_update_default_layer(default_layer);
   default_layer_set(default_layer);
@@ -172,12 +162,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   iota_gfx_flush(); // wake up screen
 #endif
 
+  if (record->event.pressed) {
+    set_keylog(keycode, record);
+    set_timelog();
+  }
+
+
   switch (keycode) {
     case QWERTY:
       if (record->event.pressed) {
-        #ifdef AUDIO_ENABLE
-          PLAY_SONG(tone_qwerty);
-        #endif
         persistent_default_layer_set(1UL<<_QWERTY);
       }
       return false;
@@ -365,15 +358,23 @@ void matrix_update(struct CharacterMatrix *dest,
   }
 }
 
-void render_status(struct CharacterMatrix *matrix) {
-  matrix_write(matrix, "Jorne BLE ");
-  matrix_write(matrix, IS_LEFT_HAND ? "(master)" : "(slave)");
+void matrix_render_user(struct CharacterMatrix *matrix) {
+  if (is_master) {
+    matrix_write_ln(matrix, read_layer_state());
+    matrix_write_ln(matrix, read_keylog());
+    matrix_write_ln(matrix, read_keylogs());
+    //matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui));
+    //matrix_write_ln(matrix, read_host_led_state());
+    //matrix_write_ln(matrix, read_timelog());
+  } else {
+    matrix_write(matrix, read_logo());
+  }
 }
 
 void iota_gfx_task_user(void) {
   struct CharacterMatrix matrix;
   matrix_clear(&matrix);
-  render_status(&matrix);
+  matrix_render_user(&matrix);
   matrix_update(&display, &matrix);
 }
 
